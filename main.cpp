bdf3276daf4b1482c1bc08f9980fcdd23a8494c0
itssiem <siem@siemgerritsen.nl>
2024-02-06 17:28:30 +0100

    Improved readability of output

--- main.cpp
+++ main.cpp
@@ -7,17 +7,23 @@ int main() {
     std::cout << "add(1, 2)" << std::endl;
     std::cout << add(1, 2) << std::endl;
 
+    std::cout << std::endl;
+
     std::cout << "Exercise 2" << std::endl;
     std::cout << "printEvenOrOdd(1)" << std::endl;
     printEvenOrOdd(1);
     std::cout << "printEvenOrOdd(2)" << std::endl;
     printEvenOrOdd(2);
 
+    std::cout << std::endl;
+
     std::cout << "Exercise 3" << std::endl;
     std::cout << "exponent(2, 3)" << std::endl;
     std::cout << exponent(2, 4) << std::endl;
     std::cout << "exponent(2, 8)" << std::endl;
     std::cout << exponent(2, 8) << std::endl;
+
+    std::cout << std::endl;
 
     std::cout << "Exercise 4" << std::endl;
     std::cout << "isPrime(3) PRIME" << std::endl;
@@ -27,10 +33,14 @@ int main() {
     std::cout << "isPrime(420) NOT PRIME" << std::endl;
     std::cout << isPrime(420) << std::endl;
 
+    std::cout << std::endl;
+
     std::cout << "Exercise 5" << std::endl;
     std::cout << "findIndex({4, 2, 1, 8}, 4, 1)" << std::endl;
     int exFive[] = {4, 2, 1, 8};
     std::cout << findIndex( exFive, 4, 1) << std::endl;
+
+    std::cout << std::endl;
 
     std::cout << "Exercise 6" << std::endl;
     std::cout << "sortArray({5, 4, 1, 7 ,6}, 5)" << std::endl;
@@ -39,6 +49,8 @@ int main() {
     sortArray(exSix, 5);
     printIntArray(exSix, 5);
 
+    std::cout << std::endl;
+
     std::cout << "Exercise 7" << std::endl;
     std::cout << "greatestCommonDivisor(24, 8)" << std::endl;
     std::cout << greatestCommonDivisor(24, 8) << std::endl;
@@ -46,6 +58,8 @@ int main() {
     std::cout << greatestCommonDivisor(105, 252) << std::endl;
     std::cout << "greatestCommonDivisor(13, 29)" << std::endl;
     std::cout << greatestCommonDivisor(13, 29) << std::endl;
+
+    std::cout << std::endl;
 
     std::cout << "Exercise 8" << std::endl;
     std::cout << "leastCommonMultiple(24, 8)" << std::endl;
